7b4e980fc347c6411919de6d41e6d3051243dcea
Anh Phi <hoanganhphidvl05@gmail.com>
2025-08-25 08:09:48 +0700

    fix UI

---
+++ src/app/thanh-toan/page.tsx
@@ -0,0 +1,7 @@
+import Banking_components from "@/components/Bank/Banking_components"
+
+export default function Banking() {
+  return (
+    <Banking_components />
+  )
+}
---
+++ src/components/Bank/Banking_components.tsx
@@ -0,0 +1,7 @@
+const Banking_components = () => {
+  return (
+    <div>Banking_components</div>
+  )
+}
+
+export default Banking_components
